d0cc297c54d5a438aad993d551a75c853a7b45ce
aydanbrown <mail@aydan.io>
2020-02-05 10:17:37 +1300

    Release 0.2.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cony.MixProject do
   def project do
     [
       app: :cony,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
